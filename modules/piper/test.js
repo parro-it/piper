3c7892f40b72c6685299fcfd535c7bd79eea4c0f
Andrea Parodi <a.parodi@inet2.it>
2017-10-05 10:25:32 +0200

    fixed tests

--- modules/piper/test.js
+++ modules/piper/test.js
@@ -19,12 +19,12 @@ test("result has stdin/stdout/stderr", t => {
 
 test("run various commands piping their stdin/outs", async t => {
   const results = fixture();
-  t.is(await getStream(results.stdout), "19\n");
+  t.is((await getStream(results.stdout)).trim(), "19");
 });
 
 test("stdout/err are thenable", async t => {
   const results = await fixture().stdout;
-  t.is(results.toString("utf-8"), "19\n");
+  t.is(results.toString("utf-8").trim(), "19");
 });
 
 test("pipe completes when last command completes", async t => {
@@ -34,5 +34,5 @@ test("pipe completes when last command completes", async t => {
 
 test("work when pipeline has one single command", async t => {
   const results = await piper(["echo", "ciao"]).stdout;
-  t.is(results.toString("utf-8"), "ciao\n");
+  t.is(results.toString("utf-8").trim(), "ciao");
 });
